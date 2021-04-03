a48c2ba411d1ce7eacb5d7f143b30c483c595c87
Pushpit Ojha <pushpitojha6376@gmail.com>
2021-04-03 18:26:47 +0530

    Day 3

---
+++ LeeCode_April2021/3_longestValidParentheses.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+
+    public int longestValidParentheses(String s) {
+        if(s.length()==0){
+            return 0;
+        }
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+             if(!stack.isEmpty() && s.charAt(i)==')' && s.charAt(stack.peek())=='('){
+                 stack.pop();
+             }else{
+                 stack.push(i);  
+             }
+        }
+        int index=-1;
+        int max=0;
+        for(int i:stack)
+        {
+            max=Math.max(max,i-index-1);
+            index=i;
+        }
+        max=Math.max(max,s.length()-index-1);
+        return max;
+    }
+    
+
+
+}
